8ce5eaf431b0f9d4f461ff238e61bfda48091994
Matthew Chen <mc.2green@gmail.com>
2020-02-06 10:50:08 -0500

    new folder C++, new folder for_while, and new file triangle_of_letters

---
+++ C++/for_while/triangle_of_letters.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+// Objective: When the user inputs a lower-case letter,
+// the code will output a right-justified right triangle
+// containing letters in an increasing order.
+// Example:
+// User inputs: d
+// Output:
+//    a
+//   ab
+//  abc
+// abcd
+int main(){
+    char inputLetter;
+    int lines;
+    cout<<"Please enter a lower case letter: "<<endl;
+    cin>>inputLetter;
+    lines = (int)inputLetter - 97 + 1;
+
+    for(int i = 1; i <= lines; i++){
+        for(int j = lines - i; j > 0; j--){
+            cout<<" ";
+        }
+        for(int k = 97; k < (97 + i); k++){
+            cout<<(char)k;
+        }
+        cout<<endl;
+    }
+    return 0;
+}
